0ce8f3a3f6301cf7b8fbc19753ce56b0e0a645be
zihegao <zihe.gao@gmail.com>
2025-07-18 17:37:18 -0500

    WaveguidePlotter added. VCSEL 5x5 spectrum plot

--- examples/VCSEL/VCSEL_array_5x5_thickCladding.py
+++ examples/VCSEL/VCSEL_array_5x5_thickCladding.py
@@ -25,8 +25,6 @@ class WaveguidePlotter:
         if modes_plot is None and n_plot is not None:   
             modes_plot = range(n_plot)
 
-        fig, axs = plt.subplots(1, 2)
-
         Ex0 = self.modes.get_field_interp(0, 'Ex', squeeze=True)
         Ey0 = self.modes.get_field_interp(0, 'Ey', squeeze=True)
         #vmin = np.min(np.append(np.abs(Ex0),np.abs(Ey0)))
@@ -40,8 +38,6 @@ class WaveguidePlotter:
             Hy = self.modes.get_field_interp(i_mode, 'Hy', squeeze=True)
             Hz = self.modes.get_field_interp(i_mode, 'Hz', squeeze=True)
             print('Effective index = {:.4}'.format(modes.neff[i_mode].real))
-
-            # eps_arr = self.modes.eps.get_values_in(self.modes.domain)
 
             f, axs = plt.subplots(3,2)
             f.suptitle(f'Mode Index: {i_mode}\n n_eff = {modes.neff[i_mode].real:.5f}')
@@ -104,13 +100,36 @@ class WaveguidePlotter:
             plt.tight_layout(rect=[0, 0.03, 1, 0.95])
             plt.show()
 
+    def plot_neff(self, n=None):
+        if n is None:
+            n = self.modes.neigs
+
+        neff = self.modes.neff[:n]
+        fig, axs = plt.subplots(2,1)
+        axs[0].plot(np.arange(n), neff.real, 'o-', label='Real part')
+        axs[0].set_title('Effective Index (Real Part)')
+        axs[0].set_xlabel('Mode Index')
+        axs[0].set_ylabel('n_eff (Real)')
+        axs[1].plot(np.arange(n), neff.imag, 'o-', label='Imaginary part', color='orange')
+        axs[1].set_title('Effective Index (Imaginary Part)')
+        axs[1].set_xlabel('Mode Index')
+        axs[1].set_ylabel('n_eff (Imaginary)')
+        plt.tight_layout()
+        plt.show()
+
+#%%
 ####################################################################################
 # Set up the size of the problem
 ####################################################################################
+
 plt.close('all')
+
+# VCSEL array geometry
 unit_cell_size = 3.0 # microns
-nominal_width = 1.2 # microns
+nominal_width = 1.2 # microns, width of coupling region
 separable_array_size = [5,5]
+
+# simulation parameters
 dx = 0.1 # microns
 dy = 0.1 # microns
 wavelength = 0.85 # microns
@@ -121,20 +140,19 @@ M = int(np.ceil((simulation_height)/dy)+1)
 simulation_width =  (N-1)*dx # microns
 simulation_height = (M-1)*dy # microns
 
-
 GaAs_Index = 3.4
 AlGaAsO_Index = 3.4-0.1 # large for now, will change to 0.02
 
 ixs, iys = np.meshgrid(np.arange(separable_array_size[0]),
-                       np.arange(separable_array_size[1]),
-                       indexing='ij')
+                    np.arange(separable_array_size[1]),
+                    indexing='ij')
 indices = np.stack([ixs,iys],axis=-1).reshape(-1,2)
 
 ## Combine the primitives into a epsilon structure
 eps = emopt.grid.StructuredMaterial2D(simulation_height,
-                                      simulation_width,
-                                      dx, 
-                                      dy)
+                                    simulation_width,
+                                    dx, 
+                                    dy)
 
 cavity_temp = emopt.grid.Polygon(
     xs=[-unit_cell_size/2, -unit_cell_size/2,
@@ -151,7 +169,7 @@ cavity_temp = emopt.grid.Polygon(
 cavity_temp.translate(
                         (-(separable_array_size[0]-1)/2)*unit_cell_size,
                         (-(separable_array_size[1]-1)/2)*unit_cell_size
-                      )
+                    )
 
 #iterate through array indices and add primitives into the epsilon matrix object
 cavities = []
@@ -244,88 +262,10 @@ print(f'{aprox_sparse_matrix_ram_GB=:.2f}')
 plt.close('all')
 plotting = True
 
-mode_plot = np.arange(0,50)
-# mode_plot = [0,1]
-if plotting:
-    
-    #display epsilon profile
-    eps_array = eps.get_values_in(domain)
-    plt.figure()
-    plt.imshow(np.real(eps_array),origin='lower')
-    
-    Ex0 = modes.get_field_interp(0, 'Ex', squeeze=True)
-    Ey0 = modes.get_field_interp(0, 'Ey', squeeze=True)
-    #vmin = np.min(np.append(np.abs(Ex0),np.abs(Ey0)))
-    #vmax = np.max(np.append(np.abs(Ex0),np.abs(Ey0)))
-    
-    for mode_index in mode_plot:
-        Ex = modes.get_field_interp(mode_index, 'Ex', squeeze=True)
-        Ey = modes.get_field_interp(mode_index, 'Ey', squeeze=True)
-        Ez = modes.get_field_interp(mode_index, 'Ez', squeeze=True)
-        Hx = modes.get_field_interp(mode_index, 'Hx', squeeze=True)
-        Hy = modes.get_field_interp(mode_index, 'Hy', squeeze=True)
-        Hz = modes.get_field_interp(mode_index, 'Hz', squeeze=True)
-        print('Effective index = {:.4}'.format(modes.neff[mode_index].real))
+wg = WaveguidePlotter(modes)
+wg.plot_eps()
 
-        eps_arr = eps.get_values_in(domain)
-        
-        f, axs = plt.subplots(3,2)
-        f.suptitle(f'Mode Index: {mode_index}\n n_eff = {modes.neff[mode_index].real:.5f}')
-        # First column: Ex, Ey, Ez
-        vmin = np.min(np.abs(Ex))
-        vmax = np.max(np.abs(Ex))
-        im1 = axs[0,0].imshow(np.abs(Ex),
-                            extent=[-simulation_width/2,simulation_width/2,
-                                    -simulation_height/2,simulation_height/2],
-                            vmin=vmin,
-                            vmax=vmax, cmap='hot', origin='lower')
-        axs[0,0].set_title('Ex Field Component')
-        f.colorbar(im1, ax=axs[0,0])
-        vmin = np.min(np.abs(Ey))
-        vmax = np.max(np.abs(Ey))
-        im2 = axs[1,0].imshow(np.abs(Ey),
-                            extent=[-simulation_width/2,simulation_width/2,
-                                    -simulation_height/2,simulation_height/2],
-                            vmin=vmin,
-                            vmax=vmax, cmap='hot', origin='lower')
-        axs[1,0].set_title('Ey Field Component')
-        f.colorbar(im2, ax=axs[1,0])
-        vmin = np.min(np.abs(Ez))
-        vmax = np.max(np.abs(Ez))
-        im3 = axs[2,0].imshow(np.abs(Ez),
-                            extent=[-simulation_width/2,simulation_width/2,
-                                    -simulation_height/2,simulation_height/2],
-                            vmin=vmin,
-                            vmax=vmax, cmap='hot', origin='lower')
-        axs[2,0].set_title('Ez Field Component')
-        f.colorbar(im3, ax=axs[2,0])
-        # Second column: Hx, Hy, Hz
-        vmin = np.min(np.abs(Hx))
-        vmax = np.max(np.abs(Hx))
-        im4 = axs[0,1].imshow(np.abs(Hx),
-                            extent=[-simulation_width/2,simulation_width/2,
-                                    -simulation_height/2,simulation_height/2],
-                            vmin=vmin,
-                            vmax=vmax, cmap='hot', origin='lower')
-        axs[0,1].set_title('Hx Field Component')
-        f.colorbar(im4, ax=axs[0,1])
-        vmin = np.min(np.abs(Hy))
-        vmax = np.max(np.abs(Hy))
-        im5 = axs[1,1].imshow(np.abs(Hy),
-                            extent=[-simulation_width/2,simulation_width/2,
-                                    -simulation_height/2,simulation_height/2],
-                            vmin=vmin,
-                            vmax=vmax, cmap='hot', origin='lower')
-        axs[1,1].set_title('Hy Field Component')
-        f.colorbar(im5, ax=axs[1,1])
-        vmin = np.min(np.abs(Hz))
-        vmax = np.max(np.abs(Hz))
-        im6 = axs[2,1].imshow(np.abs(Hz),
-                            extent=[-simulation_width/2,simulation_width/2,
-                                    -simulation_height/2,simulation_height/2],
-                            vmin=vmin,
-                            vmax=vmax, cmap='hot', origin='lower')
-        axs[2,1].set_title('Hz Field Component')
-        f.colorbar(im6, ax=axs[2,1])
-        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-        plt.show()
+modes_plot = np.arange(0,50)
+wg.plot_mode(2)
+wg.plot_mode(modes_plot = [45,46])
+wg.plot_neff()
